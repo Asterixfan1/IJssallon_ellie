bc4a492ea39a302ca7d0244439673920de47a6ae
Robert Winkler <rwinkler02@hotmail.com>
2024-09-17 15:43:57 +0200

    functies toegevoegd

---
+++ reclame.py
@@ -0,0 +1,5 @@
+def aanbieding_1(smaak, prijs, korting):
+    prijs_na_korting = prijs * (1 - korting)
+    uitvoer = f'''Vandaag in de aanbieding: Emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {prijs_na_korting} euro.'''
+    return uitvoer
+
