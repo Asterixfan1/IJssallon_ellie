c8254e801a00e8f410feac65f72646d129aee5ad
Robert Winkler <rwinkler02@hotmail.com>
2024-10-08 16:20:50 +0200

    Dit is voor les 10

---
+++ presentatie.py
@@ -0,0 +1,13 @@
+from helper import onderstreep
+def presenteer(totaal):
+    mijn_dict = {
+        "vis" : 10,
+        "vlees" : 25,
+        "overig" : 15
+        }
+    print (mijn_dict())
+    uitvoer = onderstreep
+    for el in uitvoer:
+        print (el)
+    totaal = 50
+    print (totaal())
